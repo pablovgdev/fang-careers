fde9b10198ae7ac3cdbcafe517ce23b1a7b8c9de
vforverdu <pablovgdev@gmail.com>
2020-12-22 17:47:11 +0100

    📦 NEW: old styles

--- components/job-card.tsx
+++ components/job-card.tsx
@@ -18,13 +18,13 @@ export default function JobCard({ job }: JobProps) {
 
 	return (
 		<>
-			<div className={styles.card} onClick={toggleDescription} style={{ backgroundColor: companyStyle.background }}>
-				<div className={styles.logo}>
+			<div className={styles.card} onClick={toggleDescription}>
+				<div className={styles.logo} style={{ backgroundColor: companyStyle.background }}>
 					<img src={companyStyle.logo} alt="logo" />
 				</div>
 				<section className={styles.section}>
-					<h3 style={{ color: companyStyle.primary }}>{job.title}</h3>
-					<div className={styles.details} style={{ color: companyStyle.secondary }}>
+					<h2 className={styles.title}>{job.title}</h2>
+					<div className={styles.details}>
 						<p className={styles.location}>{job.locations.join(" / ")}</p>
 						<p className={styles.date}>{dateFormat(job.date)}</p>
 					</div>
