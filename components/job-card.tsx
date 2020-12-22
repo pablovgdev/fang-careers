5a8d7c094fbbff2ee7fc56c8dab835e359eec80b
vforverdu <pablovgdev@gmail.com>
2020-12-22 13:38:22 +0100

    📦 NEW: toggle description

--- components/job-card.tsx
+++ components/job-card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactHtmlParser from "react-html-parser";
 import { Job } from "../models/job";
 import styles from "../styles/job-card.module.css";
@@ -10,29 +10,27 @@ interface JobProps {
 
 export default function JobCard({ job }: JobProps) {
 	const companyStyle = getCompanyStyle(job.company);
+	const [open, setOpen] = useState(false);
+
+	function toggleDescription() {
+		setOpen(!open);
+	};
 
 	return (
 		<>
-			<div className={styles.card}>
+			<div className={styles.card} onClick={toggleDescription}>
 				<div className={styles.logo} style={{ backgroundColor: companyStyle.secondary }}>
 					<img src={companyStyle.logo} alt="logo" />
 				</div>
 				<section className={styles.section}>
 					<h3>{job.title}</h3>
-					<div
-						className={styles.category}
-						style={{
-							backgroundColor: companyStyle.secondary,
-							color: companyStyle.primary
-						}}
-					>{job.category.toUpperCase()}</div>
 					<div className={styles.details}>
 						<p className={styles.location}>{job.locations.join(" / ")}</p>
 						<p className={styles.date}>{dateFormat(job.date)}</p>
 					</div>
 				</section>
 			</div>
-			<div className={styles.description}>
+			<div className={styles.description} style={{ display: open ? "block" : "none" }}>
 				<p>{ReactHtmlParser(job.description)}</p>
 			</div>
 		</>
